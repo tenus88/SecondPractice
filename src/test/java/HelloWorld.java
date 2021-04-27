eaffbbbddc25af8ca04ad18d6518b326defc949a
tenus88 <diachenkoandrei1@gmail.com>
2021-04-27 14:05:07 -0400

    commit with qwerty

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world1");
         System.out.println("HelloWorld2");
+        //qwerty
     }
 }
